db351bcd9dfc28d9b6fc34c6a8b012e8e3e0d6b0
12jyo <jyotiss1312@gmail.com>
2022-02-04 16:17:37 +0530

    next auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import '../styles/tailwind.css'
 import '../styles/globals.css'
+import {SessionProvider} from 'next-auth/react';
 
-const MyApp = ({ Component, pageProps }) => {
+const MyApp = ({ Component,  pageProps: {session, ...pageProps} }) => {
   return (
-    <div className="h-screen text-center text-gray-100 bg-gray-900">
-    <Component {...pageProps} />
-    </div>
+    <SessionProvider session={session}>
+      <div className="h-screen text-center text-gray-100 bg-gray-900">
+        <Component {...pageProps} />
+      </div>
+    </SessionProvider>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,44 @@
 import cx from "classnames";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 import styles from "../styles/Home.module.css";
 
 const Home = () => {
+  const { data: session } = useSession();
   const [todoItem, setTodoItem] = useState("");
   const [items, setItems] = useState([]);
+
+    useEffect(() => {
+      const run = async () => {
+        if (session) {
+          let response = await populateList(session.user.email);
+          setItems(response);
+        }
+      }
+      run();
+    }, [session]);
+
+  const populateList = async (email) => {
+    return await (await fetch("http://localhost:5000", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ email }),
+    })).json();
+  }
+
+  const addTodo = async (todo) => {
+    await (await fetch("http://localhost:5000/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(todo),
+    })).json();
+  }
 
   const handleEnter = (event) => {
     if (event.key === "Enter") {
@@ -16,23 +48,33 @@ const Home = () => {
 
   const handleAdd = () => {
     if (todoItem) {
+      const id = uuidv4();
       setItems([
         {
-          id: uuidv4(),
+          id,
           message: todoItem,
           done: false,
         },
         ...items,
       ]);
+      addTodo({
+        id,
+        message: todoItem,
+        done: false,
+        email: session.user.email
+      });
       setTodoItem("");
     }
-
   };
+
+  const doneToggle = async (id) => {
+    await (await fetch("http://localhost:5000/" + id)).json();
+  }
 
   const handleDone = (id) => {
     const _items = items.map((item) => {
       if (item.id === id) {
-
+        doneToggle(item.id);
         return {
           ...item,
           done: !item.done,
@@ -43,43 +85,53 @@ const Home = () => {
     setItems(_items);
   }
 
-  return (
-    <div className="w-3/4 mx-auto">
-      <div className="pt-12">
-        <h1 className="text-4x1">Todo App</h1>
+  if (session)
+    return (
+      <div className="w-3/4 mx-auto">
+        <div className="pt-12">
+          <h1 className="text-4x1">Todo App</h1>
+          <button onClick={() => signOut()}>Sign out</button>
+        </div>
+        <div className="pt-12">
+          <input type="text" value={todoItem} className="w-full rounded py-2 px-4 text-gray-900" onChange={(e) => setTodoItem(e.target.value)} onKeyDown={handleEnter} />
 
-      </div>
-      <div className="pt-12">
-        <input type="text" value={todoItem} className="w-full rounded py-2 px-4 text-gray-900" onChange={(e) => setTodoItem(e.target.value)} onKeyDown={handleEnter} />
+        </div>
 
-      </div>
+        <ul className="pt-12">
+          {items
+            .filter(({ done }) => !done)
+            .map(({ id, message }) => (
+              <li
+                key={id}
+                onClick={() => handleDone(id)}
+                className={cx(styles.item)}
+              >
+                {message}
+              </li>
+            ))}
+          {items
+            .filter(({ done }) => done)
+            .map(({ id, message }) => (
+              <li
+                key={id}
+                onClick={() => handleDone(id)}
+                className={cx(styles.item, styles.done)}
+              >
+                {message}
+              </li >
+            ))}
+        </ul >
+      </div >
+    );
+  else {
+    return (
 
-      <ul className="pt-12">
-        {items
-          .filter(({ done }) => !done)
-          .map(({ id, message }) => (
-            <li
-              key={id}
-              onClick={() => handleDone(id)}
-              className={cx(styles.item)}
-            >
-              {message}
-            </li>
-          ))}
-        {items
-          .filter(({ done }) => done)
-          .map(({ id, message }) => (
-            <li
-              key={id}
-              onClick={() => handleDone(id)}
-              className={cx(styles.item, styles.done)}
-            >
-              {message}
-            </li >
-          ))}
-      </ul >
-    </div >
-  );
+      <>
+        Not signed in <br />
+        <button onClick={() => signIn()}>Sign in</button>
+      </>
+    )
+  }
 };
 
 export default Home;
